fa2bac0655d66a5b553412508014aab5a1969381
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-20 14:33:43 +0530

    Create add_cmdline.java

---
+++ add_cmdline.java
@@ -0,0 +1,10 @@
+package javaapplication1;
+public class JavaApplication1 {
+public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int p = Integer.parseInt(args[1]);
+        int c = n + p;
+        System.out.println("The sum of "+n+" and "+p+" is "+c);
+    }
+    
+}
