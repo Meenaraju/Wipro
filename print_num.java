c8979def7bb24865eceb6e3576a3645cb779a8ce
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-21 13:48:00 +0530

    Create print_num.java

---
+++ print_num.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=1;i<=10;i++){
+				System.out.print(i+"\t");
+					
+			       
+		}
+	}
+}
