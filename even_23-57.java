646d58932304b45eb32f0350ca34daada82b4473
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-21 13:49:33 +0530

    Create even_23-57.java

---
+++ even_23-57.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		for(int i=23;i<57;i++){
+			if(i%2==0)
+				System.out.println(i);
+					
+			       
+		}
+	}
+}
