e7fdf82bc910ce4d7e6afb243df09c6c07a46eb1
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-23 23:58:18 +0530

    Create ascii_char.java

---
+++ ascii_char.java
@@ -0,0 +1,13 @@
+package javaapplication1;
+import java.util.*;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int a[] = new int[]{65,66,77,97,98};
+        for(int i=0;i<5;i++){
+        char c = (char)a[i];
+          System.out.print(c+" ");
+       
+             }
+    }
+}
