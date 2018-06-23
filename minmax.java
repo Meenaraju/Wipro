b84684958a8c569b91c0e7bafd7ae3f559ce113c
Meenaraju <35861448+Meenaraju@users.noreply.github.com>
2018-06-23 23:27:33 +0530

    Create minmax.java

---
+++ minmax.java
@@ -0,0 +1,29 @@
+package javaapplication1;
+import java.util.*;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int max = a[0];
+        for(int i=0;i<n;i++){
+           if(a[i]>max){
+               max = a[i];
+           }
+        }
+           int min = a[0];
+           for(int i=0;i<n;i++){
+               if(min>a[i]){
+                   min = a[i];
+               }
+           }
+            
+        
+        System.out.println(max);
+        System.out.println(min);
+            }
+    
+}
